5217b83a7e8642f69e63e1fc93fc31311312dad1
Alvaroo5 <145764969+Alvaroo5@users.noreply.github.com>
2024-06-17 10:06:19 +0200

    Update miproyecto-queries.sql

--- miproyecto-queries.sql
+++ miproyecto-queries.sql
@@ -1,18 +1,18 @@
--- 1. Mostrar los nombres de los equipos y el número total de goles marcados por cada uno en todos los partidos de manera descendiente.
-select e.nombre_equipo, SUM(g.minuto) as goles_marcados
-from equipo e inner join fichajes f 
-on e.nombre_equipo = f.EQUIPO_nombre_equipo inner join jugador j 
-on f.JUGADOR_dni = j.dni left join gol g 
+-- 1. Mostrar los nombres de los equipos junto con la suma de los minutos en los que sus jugadores han anotado goles y lo ordena en orden descendiente según la cantidad total de minutos de goles anotados.
+select e.nombre_equipo, SUM(g.minuto) as suma_minutos
+from equipo e inner join fichajes f
+on e.nombre_equipo = f.EQUIPO_nombre_equipo inner join jugador j
+on f.JUGADOR_dni = j.dni left join gol g
 on j.dni = g.jugador_marca
 group by e.nombre_equipo
-order by goles_marcados desc;
+order by suma_minutos desc;
 
 -- 2. Obtener el nombre del estadio y la cantidad total de partidos jugados en ese estadio donde su numero de telefono se encuentra entre 100000000 y 200000000
-select nombre_estadio, count(*) as total_partidos
+select nombre_estadio, telefono, count(*) as total_partidos
 from estadio e inner join juegan j
 on e.nombre_estadio = j.ESTADIO_nombre_estadio inner join partido p 
 on j.EQUIPO_nombre_equipo = p.EQUIPO_nombre_local or j.EQUIPO_nombre_equipo = p.EQUIPO_nombre_visitante 
-group by nombre_estadio, e.telefono 
+group by nombre_estadio 
 having e.telefono BETWEEN 100000000 and 200000000;
 
 -- 3. Obtener los jugadores que su nombre comienza por la letra 'J' y cuya tarifa es mayor que el promedio de la tarifa de todos los jugadores 
@@ -42,21 +42,21 @@ having Goles_marcados='2' and nombre_temporada='Temporada 1';
 
 -- Primera vista
 create view primera as
-	select e.nombre_equipo, SUM(g.minuto) as goles_marcados
-	from equipo e inner join fichajes f 
-	on e.nombre_equipo = f.EQUIPO_nombre_equipo inner join jugador j 
-	on f.JUGADOR_dni = j.dni left join gol g 
+	select e.nombre_equipo, SUM(g.minuto) as suma_minutos
+	from equipo e inner join fichajes f
+	on e.nombre_equipo = f.EQUIPO_nombre_equipo inner join jugador j
+	on f.JUGADOR_dni = j.dni left join gol g
 	on j.dni = g.jugador_marca
 	group by e.nombre_equipo
-	order by goles_marcados desc;
+	order by suma_minutos desc;
 
 -- Segunda vista
 create view segunda as 
-	select nombre_estadio, count(*) as total_partidos
+	select nombre_estadio, telefono, count(*) as total_partidos
 	from estadio e inner join juegan j
 	on e.nombre_estadio = j.ESTADIO_nombre_estadio inner join partido p 
 	on j.EQUIPO_nombre_equipo = p.EQUIPO_nombre_local or j.EQUIPO_nombre_equipo = p.EQUIPO_nombre_visitante 
-	group by nombre_estadio, e.telefono 
+	group by nombre_estadio 
 	having e.telefono BETWEEN 100000000 and 200000000;
 
 -- 1. Esta funcion toma el nombre del equipo como parámetro y devuelve la edad media de los jugadores del equipo
@@ -215,7 +215,7 @@ values ('43218401F', null, 'Sanchez', '2003-04-27', '123456789');
 
 
 -- 2. Creamos un trigger en el que antes de eliminar un estadio, esa eliminación se guarda 
--- en una nueva tabla (insertados)
+-- en una nueva tabla (eliminados)
 create table eliminados (
 	nombre_estadio varchar(20) primary key,
 	ciudad varchar(20),
